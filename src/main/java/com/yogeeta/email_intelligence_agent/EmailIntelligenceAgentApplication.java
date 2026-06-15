b08398144b984df1e65840eb82181707136beb0e
Yogeeta Paryani <yogeetaparyani@gmail.com>
2026-06-15 11:13:19 -0700

    Initial commit

---
+++ src/main/java/com/yogeeta/email_intelligence_agent/EmailIntelligenceAgentApplication.java
@@ -0,0 +1,13 @@
+package com.yogeeta.email_intelligence_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailIntelligenceAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailIntelligenceAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yogeeta/email_intelligence_agent/EmailIntelligenceAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogeeta.email_intelligence_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailIntelligenceAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
